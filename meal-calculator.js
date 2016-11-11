498a74eb7d7410fb6b6ac06a011611a14987ea77
Andrew Ilijic <ailijic@gmail.com>
2016-11-11 01:01:34 -0500

    Finished

--- meal-calculator.js
+++ meal-calculator.js
@@ -1,36 +1,105 @@
-start()
-function start () {
-  'use strict'
-  // Define Classes
-  const locale = {
-    new_jersey: {taxRate: 7.0/100},
-    unknown: {taxRate: 0.00/100}
+'use strict';
+class Dish {
+  constructor (name, price) {
+    this.name = name || 'MISC';
+    this.price = price || 0.00;
   }
-  let Diner = function Diner (Name, options) {
-    patron: '',
-    state: '',
-    tipRate: 17/100,  // I got this number from Google
-    itemsOrdered: [],
-    get taxRate () {
-      delete this.taxRate
-      return this.taxRate = locale[this.state]
-    },
-    get subTotal () {
-      let retAmount = 0.0
-      for (let i in this.itemsOrdered) {
-        retAmount += this.itemsOrdered[i].price
-      }
-      return retAmount
-    },
-    get tip () {
-      return this.subTotal * this.tipRate
-    },
-    get tax () {
-      return this.subTotal * this.taxRate
-    }
+}
+class Diner {
+  constructor (name) {
+    this.name = name || "";
+    this.dishList = [];
+    this.taxPercent = 8.875 / 100.0;
+    this.tipPercent = 20.00 / 100.0;
   }
-  main()
+  get dishTotal () {
+    let ret = 0.00;
+    this.dishList.forEach((dish) => {
+      ret += dish.price;
+    });
+    return ret;
+  }
+  get tax () {
+    return this.taxPercent * this.dishTotal;
+  }
+  get tip () {
+    return this.tipPercent * this.dishTotal;
+  }
+  addDish (dish) {
+    this.dishList.push(dish);
+  }
 }
-function main () {
-  var myLocale = new Locale
+class Bill {
+  constructor (party) {
+    this.dinerList = party || [];
+  }
+  printSubtotal () {
+    let printVal = 0.00;
+    this.dinerList.forEach((diner) => {
+      printVal += diner.dishTotal;
+      printVal += diner.tax;
+    });
+    const valString = moneyFormat(printVal);
+    console.log("Total: " + valString);
+  }
+  printTipTotal () {
+    let printVal = 0.00;
+    this.dinerList.forEach((diner) => {
+      printVal += diner.tip;
+    });
+    const valString = moneyFormat(printVal);
+    console.log("Tip: " + valString);
+  }
+  printBillSplit () {
+    console.log('Breakdown:');
+    this.dinerList.forEach((diner) => {
+      const subTotal = moneyFormat(diner.dishTotal);
+      const taxAmt = moneyFormat(diner.tax);
+      const tipAmt = moneyFormat(diner.tip);
+      const grandTotalNum = diner.dishTotal 
+        + diner.tax + diner.tip;
+      const grandTotal = moneyFormat(grandTotalNum);
+      console.log(diner.name + ':');
+      console.log('  Subtotal: ' + subTotal);
+      console.log('  Tax: ' + taxAmt);
+      console.log('  Tip: ' + tipAmt);
+      console.log('  ---------------------------');
+      console.log('  Grand Total: ' + grandTotal);
+    });
+  }
 }
+function moneyFormat(price, signSym) {
+  const sign = signSym || '$';
+  const pieces = parseFloat(price).toFixed(2).split('');
+  let ii = pieces.length - 3;
+  while ((ii-=3) > 0) {
+    pieces.splice(ii, 0, ',');
+  }
+  return sign + pieces.join('');
+}
+// Main
+//appetizers
+const app1 = new Dish('app1', 9.99);
+const app2 = new Dish('app2', 8.88);
+const app3 = new Dish('app3', 7.77);
+const apps = [app1, app2, app3];
+//entrees
+const chicken = new Dish('chicken', 23.45);
+const fish = new Dish('fish', 22.22);
+const beef = new Dish('beef', 33.33);
+const entrees = [chicken, fish, beef];
+//Diners 
+let peter = new Diner('Peter');
+let paul = new Diner('Paul');
+let mary = new Diner('Mary');
+const party = [peter, paul, mary];
+// Add dishes to Diners
+party.forEach((diner, index) => {
+  diner.addDish(apps[index]);
+  diner.addDish(entrees[index]);
+});
+// Print Bill
+const table1 = new Bill(party);
+table1.printSubtotal();
+table1.printTipTotal();
+table1.printBillSplit();
